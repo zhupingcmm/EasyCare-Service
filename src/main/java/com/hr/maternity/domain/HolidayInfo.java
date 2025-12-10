d92b73c83a08f9cd191468e67df3b7512867d301
tomastao <tomastao@hotmail.com>
2025-12-11 00:18:28 +0800

    重构refund

---
+++ src/main/java/com/hr/maternity/domain/HolidayInfo.java
@@ -0,0 +1,29 @@
+package com.hr.maternity.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * 节假日信息
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HolidayInfo {
+    /** 日期 */
+    private LocalDate date;
+    
+    /** 节假日名称 */
+    private String name;
+    
+    /** 是否为法定假日 */
+    private Boolean isPublicHoliday;
+    
+    /** 类型：public_holiday 或 transfer_workday */
+    private String type;
+}
