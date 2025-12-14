b2b934d4cbb5a1a4f9d75ea757b8345762508916
挥书 <lazygirlw@163.com>
2025-12-14 17:33:27 +0800

    rm redundant code

--- src/main/java/com/ocbc/ms/easy/care/strategy/leave/MaternityLeaveDaysStrategy.java
+++ src/main/java/com/ocbc/ms/easy/care/strategy/leave/MaternityLeaveDaysStrategy.java
@@ -1,12 +1,36 @@
 package com.ocbc.ms.easy.care.strategy.leave;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.ocbc.ms.easy.care.dto.MaternityLeaveExtDTO;
 import com.ocbc.ms.easy.care.dto.MaternityLeaveRequest;
 import com.ocbc.ms.easy.care.entity.MaternityRules;
 import com.ocbc.ms.easy.care.enums.MaternityLeaveTypeEnum;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
 
 public interface MaternityLeaveDaysStrategy {
 
     MaternityLeaveTypeEnum getType();
 
     Integer calculate(MaternityLeaveRequest request, MaternityRules rule);
+
+
+    default Integer findDaysFromExtOrDefault(MaternityRules rule, String matchCode) {
+        Object ext = rule.getMaternityLeaveExt();
+        if (ext == null) {
+            return rule.getDefaultDays();
+        }
+        List<MaternityLeaveExtDTO> extList = JSONArray.parseArray((String) ext, MaternityLeaveExtDTO.class);
+        if (CollectionUtils.isEmpty(extList)) {
+            return rule.getDefaultDays();
+        }
+        for (MaternityLeaveExtDTO dto : extList) {
+            if (dto != null && Objects.equals(dto.getCode(), matchCode)) {
+                return dto.getDays();
+            }
+        }
+        return rule.getDefaultDays();
+    }
 }
